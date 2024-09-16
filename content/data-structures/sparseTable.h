8aeb3402a138479b771cf58b927812b462737833
dothething <165024975+dothething1DA@users.noreply.github.com>
2024-09-16 19:51:28 +1000

    Update and rename sparse_table.cpp to sparseTable.h

--- content/data-structures/sparseTable.h
+++ content/data-structures/sparseTable.h
@@ -1,5 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
+/**
+ * Author: me
+ * Date: 2024-09-16
+ * License: CC0
+ * Source: https://https://github.com/dothething1DA/cp_template/edit/main/content/data-structures/sparseTable.h
+ * Description: Sparse table for RMQ.
+ * Status: Tested on SPOJ FASTFLOW and SPOJ MATCHING, stress-tested
+ */
 
 // Remember to check the limits
 // Remember to check the min-max
