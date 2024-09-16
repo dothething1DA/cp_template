c3da08b13676dc6eb0609b1a3c1d56c5f0484a7f
dothething <165024975+dothething1DA@users.noreply.github.com>
2024-09-16 17:46:24 +1000

    Update UnionFind.h

--- content/data-structures/UnionFind.h
+++ content/data-structures/UnionFind.h
@@ -8,17 +8,54 @@
  */
 #pragma once
 
-struct UF {
-	vi e;
-	UF(int n) : e(n, -1) {}
-	bool sameSet(int a, int b) { return find(a) == find(b); }
-	int size(int x) { return -e[find(x)]; }
-	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
-	bool join(int a, int b) {
-		a = find(a), b = find(b);
-		if (a == b) return false;
-		if (e[a] > e[b]) swap(a, b);
-		e[a] += e[b]; e[b] = a;
-		return true;
+// Remember to check limit
+struct DSU {
+	static constexpr int N_MAX = 2e5 + 1; //(*)
+
+	int n, t;
+	int par[N_MAX];
+	int temp[N_MAX];
+
+	DSU() {}
+	void init(int _n) {
+		n = _n;
+		for (int i = 0; i <= n; i += 1) par[i] = -1;
+	}
+
+	int findRoot(int vt) {
+		t = 0;
+		while (par[vt] >= 0) {
+			temp[t++] = vt;
+			vt = par[vt];
+		}
+		for (int i = 0; i < t; i += 1) par[temp[i]] = vt;
+		return vt;
+	}
+
+	void join(int u, int v) {
+		u = findRoot(u);
+		v = findRoot(v);
+		if (u == v) return;
+		if (par[u] > par[v]) {u^=v;v^=u;u^=v;}
+		par[u] += par[v];
+		par[v] = u;
+	}
+
+	bool sameSet(int u, int v) {
+		return findRoot(u) == findRoot(v);
 	}
 };
+
+DSU dsu;
+
+int main() {
+	int n, q; cin >> n >> q;
+	dsu.init(n);
+
+	int t, u, v;
+	for (int i = 0; i < q; i += 1) {
+		cin >> t >> u >> v;
+		if (!t) dsu.join(u, v);
+		else cout << dsu.sameSet(u, v) << "\n";
+	}
+}
